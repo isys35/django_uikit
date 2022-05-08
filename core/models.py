eeb853618f6711b345d99b3c4a68d74666c8a7e6
Дмитрий <55105946+isys35@users.noreply.github.com>
2022-05-08 18:08:46 +0300

    feat: add custom user

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from core.managers import UserManager
+
+
+class MyUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
