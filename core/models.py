03fa519477cdf2632d72236a7d36361f82150371
Дмитрий <55105946+isys35@users.noreply.github.com>
2022-05-08 18:24:44 +0300

    fix: fix custom user

--- core/models.py
+++ core/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import PermissionsMixin
 from core.managers import UserManager
 
 
-class MyUser(AbstractUser):
+class GymUser(AbstractUser, PermissionsMixin):
     username = None
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
--- gym/settings.py
+++ gym/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # myapps
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'core.GymUser'
