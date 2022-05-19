0d797823ab33bbbcb93fe3e8082eb32021dbb579
Дмитрий <55105946+isys35@users.noreply.github.com>
2022-05-19 21:27:00 +0300

    🛠 REFACTOR: Рефакторинг моделей

--- trener/models.py
+++ trener/models.py
@@ -26,7 +26,7 @@ class UserExercise(DateTimeMixin, ExerciseBase):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-class Exercises(models.Model):
+class CommonExercise(models.Model):
     exercise = models.ForeignKey(
         Exercise,
         on_delete=models.CASCADE,
@@ -40,12 +40,6 @@ class Exercises(models.Model):
         null=True
     )
 
-    workout = models.ForeignKey(
-        'Workout',
-        on_delete=models.CASCADE,
-        related_name='exercise_workout'
-    )
-
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(exercise__isnull=False) or models.Q(user_exercise__isnull=False),
@@ -53,5 +47,29 @@ class Exercises(models.Model):
         ]
 
 
+class ExerciseWorkout(DateTimeMixin, models.Model):
+    exercise = models.ForeignKey(CommonExercise,
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 null=True)
+    workout = models.ForeignKey(
+        'Workout',
+        on_delete=models.CASCADE,
+        related_name='exercise_workout',
+        blank=True,
+        null=True
+    )
+    finished = models.BooleanField(default=False)
+    date_finish = models.DateTimeField(blank=True, null=True, default=None)
+
+
+class ExerciseSet(models.Model):
+    exercise_workout = models.ForeignKey(ExerciseWorkout, on_delete=models.CASCADE, related_name='sets')
+    count_index = models.IntegerField(default=1)
+    repeat = models.IntegerField(default=0)
+    mass = models.IntegerField(default=0)
+
+
 class Workout(DateTimeMixin, models.Model):
-    exercises = models.ManyToManyField(Exercise, through=Exercises)
+    exercises = models.ManyToManyField(CommonExercise, through=ExerciseWorkout)
+    date = models.DateTimeField(blank=True, null=True, default=None)
